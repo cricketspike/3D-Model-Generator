0741cd197cbadbe5daefb0c4525e18faf5502a9c
andrewjdato <adato@ucsc.edu>
2016-10-13 18:03:45 -0700

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
+#include "src/importside.h"
 #include "src/mainwaindow.h"
 
 int main(int argc, char **argv)
@@ -12,6 +14,7 @@ int main(int argc, char **argv)
     std::cout << "-----------------" << std::endl;
     std::cout << "Commands: hello  " << std::endl;
     std::cout << "          render " << std::endl;
+    std::cout << "          image " << std::endl;
     std::cout << "          quit   " << std::endl << std::endl;
 
     while (true) {
@@ -45,6 +48,27 @@ int main(int argc, char **argv)
 
             app.setQuitOnLastWindowClosed(true);
             app.exec();
+
+        } else if (input == "image") {
+
+            QImage img0( ":/images/image0.jpg" );
+            QImage img1( ":/images/image1.jpg" );
+            QImage img2( ":/images/image2.jpg" );
+            QImage img3( ":/images/image3.jpg" );
+            QImage img4( ":/images/image4.jpg" );
+            QImage img5( ":/images/image5.jpg" );
+
+            vector<QImage> images;
+            images.push_back(img0);
+            images.push_back(img1);
+            images.push_back(img2);
+            images.push_back(img3);
+            images.push_back(img4);
+            images.push_back(img5);
+
+            box b = new box();
+            b.setUp(images);
+
 
         } else if (input == "exit" || input == "quit") {
 
